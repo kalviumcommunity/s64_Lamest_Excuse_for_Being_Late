cd27d993dc9a4a1445b109b8b97f82629f7359d1
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-02-07 11:39:48 +0530

    milestone7

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const dataBase = require('./dataBase');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config();
 
+
 // Route for /ping with basic error handling
-app.get('/ping', (req, res) => {
+app.get('/', (req, res) => {
     try {
-        res.send('Pong!');
+        res.send('Congratulation your.... hmmm(I mean Harsh) your database is connected!');
     } catch (error) {
         res.status(500).send('An error occurred!');
     }
 });
 
 dataBase();
+
+
 // Use an environment variable for the port with a fallback to 8000
 const PORT = process.env.PORT || 8000;
 
