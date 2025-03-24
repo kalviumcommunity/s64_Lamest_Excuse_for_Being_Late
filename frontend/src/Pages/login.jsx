633ae814d43545ed93c0a32a7ec19cc758082ef7
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-03-24 11:10:11 +0530

    Debuging global login & Signup

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3001/api/auth/login", {
+      const response =await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`,  {
         email: formData.email,
         password: formData.password,
       });
